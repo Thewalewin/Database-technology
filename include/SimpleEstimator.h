3f28dafda51df4e5bf5d094d0ba7039c23592700
Stephan Oostveen <s.p.o.oostveen@student.tue.nl>
2018-03-24 22:09:18 +0100

    First attempt at lecture implementation

--- include/SimpleEstimator.h
+++ include/SimpleEstimator.h
@@ -8,59 +8,20 @@
 #include "Estimator.h"
 #include "SimpleGraph.h"
 
-template<int N>
-struct Factorial {
-	static constexpr uint64_t result = N * Factorial<N-1>::result;
-};
-
-template<>
-struct Factorial<0> {
-	static constexpr uint64_t result = 1; 
-};
-
 class SimpleEstimator : public Estimator {
 
     std::shared_ptr<SimpleGraph> graph;
-	static constexpr uint64_t factorials[21] = {
-		Factorial<0>::result,
-		Factorial<1>::result,
-		Factorial<2>::result,
-		Factorial<3>::result,
-		Factorial<4>::result,
-		Factorial<5>::result,
-		Factorial<6>::result,
-		Factorial<7>::result,
-		Factorial<8>::result,
-		Factorial<9>::result,
-		Factorial<10>::result,
-		Factorial<11>::result,
-		Factorial<12>::result,
-		Factorial<13>::result,
-		Factorial<14>::result,
-		Factorial<15>::result,
-		Factorial<16>::result,
-		Factorial<17>::result,
-		Factorial<18>::result,
-		Factorial<19>::result,
-		Factorial<20>::result,
+
+	std::vector<uint32_t> labelSums;
+	std::vector<double> labelDistribution;
+	
+	struct EstimatorPair {
+		uint32_t leftLabel;
+		uint32_t rightLabel;
+		uint32_t cardinalityEstimate;
 	};
-	
-	uint32_t nrOfStartVertices = 0;
-	uint32_t nrOfEndVertices = 0;
-	uint32_t nrOfVertices = 0;
-	uint32_t nrOfEdges = 0;
-	uint32_t maxOutDegree = 0;
-	uint32_t nrOfZeroOutDegree = 0;
-	uint32_t pathLength = 0;
-	
-	uint32_t getMaxOutDegree();
-	uint32_t getZeroDegree();
-	uint32_t getPathLength(RPQTree *q);
-	uint32_t countVertices(uint32_t label, bool inverse);
-	uint32_t getNrOfStartVertices(RPQTree *q);
-	uint32_t getNrOfEndVertices(RPQTree *q);
-	double probLabel(uint32_t pathLength);
-	double pathSum(uint32_t pathLength);
+
+	EstimatorPair estimate_aux(RPQTree *q);
 public:
     explicit SimpleEstimator(std::shared_ptr<SimpleGraph> &g);
     ~SimpleEstimator() = default;
--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -6,181 +6,78 @@
 #include "SimpleEstimator.h"
 #include <cmath>
 SimpleEstimator::SimpleEstimator(std::shared_ptr<SimpleGraph> &g){
-
-    // works only with SimpleGraph
     graph = g;
 }
 
-uint32_t SimpleEstimator::getMaxOutDegree() {
-	uint32_t max = 0;
-	for (const auto &l : graph->adj) {
-		max = std::max(max, static_cast<uint32_t>(l.size()));
-	}
-	return max;
-}
-
-uint32_t SimpleEstimator::getZeroDegree() {
-	//The nr of vertices with zero outgoing edges, is the nr of vertices - the vertices with at least one outgoing edge
-	// return graph->getNoVertices() - graph->adj.size();
-	return 0; // Apparently there cannot be vertices with zero out degree
-}
-
 void SimpleEstimator::prepare() {
-	// Initialize the statistics that depend on the graph.
-	nrOfVertices = graph->getNoVertices();
-	nrOfEdges = graph->getNoEdges();
-	maxOutDegree = getMaxOutDegree();
-	nrOfZeroOutDegree = getZeroDegree();
+	// queryPath.clear();
+	// pathLength = 0;
+	uint32_t noLabels = graph->getNoLabels();
+	labelSums.resize(noLabels);
+	labelDistribution.resize(noLabels);
+
+	for (auto source : graph->adj) {
+		for (auto pair : source) {
+			labelSums[pair.first] = labelSums[pair.first] + 1;
+		}
+	}
+
+	for (uint32_t i; i < noLabels; i++) {
+		labelDistribution[i] = (double)labelSums[i] / (double)graph->getNoEdges();
+	}
 }
 
-//Get the path length of the query. This can be done using some regex tricks on the RPQTree string.
-uint32_t SimpleEstimator::getPathLength(RPQTree *q) {
-	if (q->isLeaf()){
+/*
+ * Retreive the queried path and put it into a vector, which makes it
+ * easier to perform the estimations based on the path. 
+ */
+SimpleEstimator::EstimatorPair SimpleEstimator::estimate_aux(RPQTree *q) {
+	if(q == nullptr) {
+		return {0,0,0};
+	}
+	if(q->isLeaf()) {
 		std::regex directLabel (R"((\d+)\+)");
-		std::regex inverseLabel (R"((\d+)\-)");
+        std::regex inverseLabel (R"((\d+)\-)");
+
+        std::smatch matches;
+
+        EstimatorPair ep;
+
+        if(std::regex_search(q->data, matches, directLabel)) {
+            ep.leftLabel = (uint32_t) std::stoul(matches[1]);
+        } else if(std::regex_search(q->data, matches, inverseLabel)) {
+            ep.leftLabel = (uint32_t) std::stoul(matches[1]);
+        } else {
+			ep.leftLabel = 0;
+            std::cerr << "Label parsing failed!" << std::endl;
+        }
+		ep.rightLabel = ep.leftLabel;
+		ep.cardinalityEstimate = labelSums[ep.leftLabel];
+		return ep;
+	}
+
+	if(q->isConcat()){
+		EstimatorPair left = estimate_aux(q->left);
+		EstimatorPair right = estimate_aux(q->right);
+		EstimatorPair join;
 		
-		std::smatch matches;
-		uint32_t length = 0;
-		
-		if(std::regex_search(q->data, matches, directLabel)) {
-			length = matches.size();
-		} else if(std::regex_search(q->data, matches, inverseLabel)) {
-			length = matches.size();
-		}
-		return length;
+		uint32_t Tleft = left.cardinalityEstimate;
+		double Vleft = 1.0f;
+		uint32_t Tright = right.cardinalityEstimate;
+		double Vright = labelDistribution[right.leftLabel];
+
+		join.leftLabel = left.leftLabel;
+		join.rightLabel = right.rightLabel;
+		join.cardinalityEstimate = std::min(Tright * Tleft * Vleft, 
+											Tleft * Tright * Vright);
+		return join;
 	}
-	if (q->isConcat()) {
-		uint32_t length = 0;
-		length += getPathLength(q->left);
-		length += getPathLength(q->right);
-		return length;
-	}
-	return 0;
-}
-
-//Counts the amount of vertices with the given label
-uint32_t SimpleEstimator::countVertices(uint32_t label, bool inverse) {
-	uint32_t count = 0;
-	if(!inverse) {
-		//going forward
-		for (uint32_t source = 0; source < graph->getNoVertices(); source++){
-			for (auto labelTarget : graph->adj[source]) {
-				if(labelTarget.first == label) {
-					count++;
-					break; // We break because we only count the amount of vertices.
-				}
-			}
-		}
-	} else {
-		//going backward
-		for (uint32_t source = 0; source < graph->getNoVertices(); source++){
-			for (auto labelTarget : graph->reverse_adj[source]) {
-				if (labelTarget.first == label) {
-					count++;
-					break; // We break because we only count the amount of vertices.
-				}
-			}
-		}
-	}
-	return count;
-}
-
-//Gets the nr of vertices that match the first query label
-uint32_t SimpleEstimator::getNrOfStartVertices(RPQTree *q) {
-	RPQTree *tree = q;
-	if(tree->left != nullptr){
-		while (!tree->left->isLeaf()) {
-			tree = tree->left;
-		}
-		if (!tree->isLeaf()){
-			tree = tree->left;
-		}
-	}
-	std::regex directLabel (R"((\d+)\+)");
-	std::regex inverseLabel (R"((\d+)\-)");
-
-	std::smatch matches;
-
-	uint32_t label;
-	bool inverse;
-
-	if(std::regex_search(tree->data, matches, directLabel)) {
-		label = (uint32_t) std::stoul(matches[1]);
-		inverse = false;
-	} else if(std::regex_search(tree->data, matches, inverseLabel)) {
-		label = (uint32_t) std::stoul(matches[1]);
-		inverse = true;
-	} else {
-		std::cerr << "Label parsing failed!" << std::endl;
-	}
-	return countVertices(label, inverse);
-}
-
-//gets the nr of vertices that match the last query label
-uint32_t SimpleEstimator::getNrOfEndVertices(RPQTree *q) {
-	RPQTree *tree = q;
-	if (tree->right != nullptr){
-		while (!tree->right->isLeaf()) {
-			tree = tree->right;
-		}
-		if(!tree->isLeaf()){
-			tree = tree->right;
-		}
-	}
-	std::regex directLabel (R"((\d+)\+)");
-	std::regex inverseLabel (R"((\d+)\-)");
-
-	std::smatch matches;
-
-	uint32_t label;
-	bool inverse;
-
-	if(std::regex_search(tree->data, matches, directLabel)) {
-		label = (uint32_t) std::stoul(matches[1]);
-		inverse = false;
-	} else if(std::regex_search(tree->data, matches, inverseLabel)) {
-		label = (uint32_t) std::stoul(matches[1]);
-		inverse = true;
-	} else {
-		std::cerr << "Label parsing failed!" << std::endl;
-	}
-	return countVertices(label, inverse);
-}
-
-constexpr uint64_t SimpleEstimator::factorials[];
-
-double SimpleEstimator::probLabel(uint32_t pathLength){
-	uint32_t distinctLabels = graph->getNoLabels();
-	double avgPerLabel = (1.0f / distinctLabels) * nrOfEdges;
-	double prob = 0;
-	for (uint32_t i = 1; i <= pathLength; i++) {
-		double fact = (factorials[pathLength]/ (factorials[i] * factorials[pathLength - i]));
-		double j = std::pow(avgPerLabel/nrOfEdges, i);
-		double k = std::pow((nrOfEdges-avgPerLabel)/nrOfEdges, pathLength - i);
-		prob += fact * j * k;
-	}
-	if (prob < 0.0 || prob > 1.0){
-		std::cout << "Error probability incorrect: p = " << prob;
-		return 0;
-	}
-	return prob;
-}
-
-double SimpleEstimator::pathSum(uint32_t pathLength) {
-	double sum = 0;
-	double avg = nrOfEdges / nrOfVertices;
-	for (uint32_t i = 1; i <= pathLength; i++) {
-		sum += (pathLength + 1) * std::pow(avg + (avg - maxOutDegree)/(nrOfVertices-1), i) * (1 -probLabel(pathLength));
-	}
-	return sum;
 }
 
 cardStat SimpleEstimator::estimate(RPQTree *q) {
+	// queryPath.clear();
+	// pathLength = 0;
+	EstimatorPair result = estimate_aux(q);
 
-    // perform your estimation here
-	pathLength = getPathLength(q)/2;
-	nrOfStartVertices = getNrOfStartVertices(q);
-	nrOfEndVertices = getNrOfEndVertices(q);
-	uint32_t pathCardinality = nrOfStartVertices * nrOfEndVertices * pathSum(pathLength);
-    return cardStat {nrOfStartVertices, pathCardinality, nrOfEndVertices};
+    return cardStat {labelSums[result.leftLabel], result.cardinalityEstimate, labelSums[result.rightLabel]};
 }
